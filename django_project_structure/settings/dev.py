b5a60aa8841ee16bd4e723c5a3e3051830f76198
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-08-19 22:22:37 +0600

    env setup and media setup

--- django_project_structure/settings/dev.py
+++ django_project_structure/settings/dev.py
@@ -1,1 +1,27 @@
 from .base import *
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
+PRODUCTION = os.environ.get('PRODUCTION')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/' 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/', 'staticroot/')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/' 'media')
+
+
+
+ 
--- django_project_structure/settings/prod.py
+++ django_project_structure/settings/prod.py
@@ -1,1 +1,25 @@
 from .base import *
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+PRODUCTION = os.getenv('PRODUCTION')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
